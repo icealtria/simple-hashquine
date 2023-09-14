d5d464b2e00645e8380eeae19fba3064b1449d4b
Rin <icealtria+git@gmail.com>
2023-09-14 18:50:24 +0800

    10x

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io::Write;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use std::thread;
 use std::{env, sync::mpsc};
 
@@ -5,31 +8,52 @@ use sha2::{Digest, Sha256};
 
 fn find_matching_hash(message: &str, prefix_length: u32) -> Option<String> {
     let (tx, rx) = mpsc::channel();
-    let num_threads = num_cpus::get();
+    let num_threads = num_cpus::get() as u64;
+    let found = Arc::new(AtomicBool::new(false));
 
-    for _ in 0..num_threads {
+    for thread_id in 0..num_threads {
         let tx = tx.clone();
         let message = message.to_owned();
+        let found = Arc::clone(&found);
+
         thread::spawn(move || {
-            let mut counter: u64 = 0;
             let target = 16u64.pow(prefix_length);
 
-            while counter < target {
-                let format_hash = format!("{:0width$x}", counter, width = prefix_length as usize);
-                let msg = format!("{} {}", message, format_hash);
+            // Calculate the range of counters for this thread
+            let start = thread_id * (target / num_threads);
+            let end = if thread_id == num_threads - 1 {
+                target
+            } else {
+                (thread_id + 1) * (target / num_threads)
+            };
 
-                let mut hasher = Sha256::new();
-                hasher.update(msg.as_bytes());
-                let hash_value = hasher.finalize();
-                let hash_string = hex::encode(hash_value);
-                let hash_prefix = &hash_string[..prefix_length as usize];
+            let mut hasher = Sha256::new();
+            let mut buffer = Vec::with_capacity(message.len() + prefix_length as usize);
 
-                if hash_prefix == format_hash {
-                    tx.send(Some(msg)).unwrap();
+            for counter in start..end {
+                if found.load(Ordering::Relaxed) {
                     return;
                 }
 
-                counter += 1;
+                buffer.clear();
+                write!(
+                    &mut buffer,
+                    "{} {:0width$x}",
+                    message,
+                    counter,
+                    width = prefix_length as usize
+                )
+                .unwrap();
+
+                hasher.update(&buffer);
+                let hash_string = hex::encode(hasher.finalize_reset());
+                let hash_prefix = &hash_string[..prefix_length as usize];
+
+                if &buffer[message.len() + 1..] == hash_prefix.as_bytes() {
+                    tx.send(Some(String::from_utf8(buffer).unwrap())).unwrap();
+                    found.store(true, Ordering::Relaxed);
+                    return;
+                }
             }
 
             tx.send(None).unwrap();
